701a69dceab02b81c5fb6dd3ef1d1e6aa262b5df
Calvin <calvinmdelrosario@gmail.com>
2025-02-22 07:50:14 +0800

    Added tenant_id in fulfillment_inbound

--- amazon_fulfillment_inbound.py
+++ amazon_fulfillment_inbound.py
@@ -13,15 +13,17 @@ import logger_setup
 logger_setup.setup_logging(__file__)
 logger = logging.getLogger(__name__)
 
+tenants = postgresql.get_tenants()
+
 
 @throttle_retry()
 @load_all_pages(extras=dict(QueryType='NEXT_TOKEN'))
-def load_all_shipments(marketplace='US', **kwargs):
+def load_all_shipments(account='Bare Barrel', marketplace='US', **kwargs):
     """
     a generator function to return all pages, obtained by NextToken
     """
     response = FulfillmentInbound(
-                            account=marketplace,
+                            account=f'{account}-{marketplace}',
                             marketplace=Marketplaces[marketplace]
                             ).get_shipments(**kwargs)
     return response
@@ -29,12 +31,12 @@ def load_all_shipments(marketplace='US', **kwargs):
 
 @throttle_retry()
 @load_all_pages(extras=dict(QueryType='NEXT_TOKEN'))
-def load_all_shipment_items(marketplace='US', **kwargs):
+def load_all_shipment_items(account='Bare Barrel', marketplace='US', **kwargs):
     """
     a generator function to return all pages, obtained by NextToken
     """
     response = FulfillmentInbound(
-                            account=marketplace,
+                            account=f'{account}-{marketplace}',
                             marketplace=Marketplaces[marketplace]
                             ).shipment_items(**kwargs)
     return response
@@ -42,19 +44,19 @@ def load_all_shipment_items(marketplace='US', **kwargs):
 
 @throttle_retry()
 @load_all_pages(next_token_param='paginationToken')
-def load_all_inbound_plans(marketplace='US', **kwargs):
+def load_all_inbound_plans(account='Bare Barrel', marketplace='US', **kwargs):
     """
     a generator function to return all pages, obtained by NextToken
     """
     response = FulfillmentInbound(
-                            account=marketplace,
+                            account=f'{account}-{marketplace}',
                             marketplace=Marketplaces[marketplace],
                             version='2024-03-20'
                             ).list_inbound_plans(**kwargs)
     return response
 
 
-def get_all_shipments(marketplaces=['US', 'UK'], **kwargs):
+def get_all_shipments(account='Bare Barrel', marketplaces=['US', 'UK'], **kwargs):
     """
     Gets & combines all inbound shipments top level details. Uses version v0.
     Marketplaces are by aggregated by region. E.g. US & CA are the the same.
@@ -76,8 +78,8 @@ def get_all_shipments(marketplaces=['US', 'UK'], **kwargs):
     shipments_data = pd.DataFrame()
 
     for marketplace in to_list(marketplaces):
-        logger.info(f'Retrieving Fulfillment Inbound Shipments from {marketplace} \n {kwargs}')
-        response = load_all_shipments(marketplace, **kwargs)
+        logger.info(f'Retrieving Fulfillment Inbound Shipments from {account}-{marketplace} \n {kwargs}')
+        response = load_all_shipments(account, marketplace, **kwargs)
 
         page_no = 0
         for page in response:
@@ -86,13 +88,14 @@ def get_all_shipments(marketplaces=['US', 'UK'], **kwargs):
             payload = page.payload.get('ShipmentData')
             data = pd.json_normalize(payload)
             # data.insert(0, 'marketplace', marketplace) # Marketplaces are aggregated by region
+            data['tenant_id'] = tenants[account]
             shipments_data = pd.concat([shipments_data, data], ignore_index=True)
             time.sleep(0.5)
 
     return shipments_data
 
 
-def get_all_shipment_items(marketplaces=['US', 'UK'], **kwargs):
+def get_all_shipment_items(account='Bare Barrel', marketplaces=['US', 'UK'], **kwargs):
     """
     Gets & combines all inbound shipments items. Uses version v0.
     Marketplaces are by aggregated by region. E.g. US & CA are the the same.
@@ -113,8 +116,8 @@ def get_all_shipment_items(marketplaces=['US', 'UK'], **kwargs):
     shipments_data = pd.DataFrame()
 
     for marketplace in to_list(marketplaces):
-        logger.info(f'Retrieving Fulfillment Inbound Shipment Items from {marketplace} \n {kwargs}')
-        response = load_all_shipment_items(marketplace, **kwargs)
+        logger.info(f'Retrieving Fulfillment Inbound Shipment Items from {account}-{marketplace} \n {kwargs}')
+        response = load_all_shipment_items(account, marketplace, **kwargs)
 
         page_no = 0
         for page in response:
@@ -123,6 +126,7 @@ def get_all_shipment_items(marketplaces=['US', 'UK'], **kwargs):
             payload = page.payload.get('ItemData')
             data = pd.json_normalize(payload)
             # data.insert(0, 'marketplace', marketplace) # Marketplaces are aggregated by region
+            data['tenant_id'] = tenants[account]
             shipments_data = pd.concat([shipments_data, data], ignore_index=True)
             time.sleep(0.5)
 
@@ -130,7 +134,7 @@ def get_all_shipment_items(marketplaces=['US', 'UK'], **kwargs):
 
 
 
-def get_all_inbound_plans(marketplaces=['US', 'UK'], statuses=['ACTIVE', 'VOIDED', 'SHIPPED'], **kwargs):
+def get_all_inbound_plans(account='Bare Barrel', marketplaces=['US', 'UK'], statuses=['ACTIVE', 'VOIDED', 'SHIPPED'], **kwargs):
     """
     Provides a list of inbound plans with minimal information.
     Gets and combined all inbound plans. Uses version 2024-03-20.
@@ -157,9 +161,9 @@ def get_all_inbound_plans(marketplaces=['US', 'UK'], statuses=['ACTIVE', 'VOIDED
     for marketplace in to_list(marketplaces):
 
         for status in to_list(statuses):
-            logger.info(f'Retrieving Inbound Plans ({status}) from {marketplace} \n {kwargs}')
+            logger.info(f'Retrieving Inbound Plans ({status}) from {account}-{marketplace} \n {kwargs}')
 
-            response = load_all_inbound_plans(marketplace, status=status, **kwargs)
+            response = load_all_inbound_plans(account, marketplace, status=status, **kwargs)
 
             page_no = 0
             for page in response:
@@ -168,6 +172,7 @@ def get_all_inbound_plans(marketplaces=['US', 'UK'], statuses=['ACTIVE', 'VOIDED
                 payload = page.payload.get('inboundPlans')
                 data = pd.json_normalize(payload)
                 # data.insert(0, 'marketplace', marketplace) # Marketplaces are aggregated by region
+                data['tenant_id'] = tenants[account]
                 inbound_data = pd.concat([inbound_data, data], ignore_index=True)
                 time.sleep(0.5)
 
@@ -176,7 +181,7 @@ def get_all_inbound_plans(marketplaces=['US', 'UK'], statuses=['ACTIVE', 'VOIDED
     return inbound_data
 
 
-def get_all_inbound_plans_info(marketplaces=['US', 'UK'], inbound_plan_ids='All', last_updated_at='1900-01-01'):
+def get_all_inbound_plans_info(account='Bare Barrel', marketplaces=['US', 'UK'], inbound_plan_ids='All', last_updated_at='1900-01-01'):
     """
     Fetches and combines the top level information of inbound plans.
     The shipment_id inside shipments can used to retrieve FBA shipment details in getShipment.
@@ -193,7 +198,7 @@ def get_all_inbound_plans_info(marketplaces=['US', 'UK'], inbound_plan_ids='All'
     inbound_data = pd.DataFrame()
 
     for marketplace in to_list(marketplaces):
-        logger.info(f"Fetching top level information of inbound plans from {marketplace}")
+        logger.info(f"Fetching top level information of inbound plans from {account}-{marketplace}")
 
         if inbound_plan_ids == 'All':
             logger.info(f"Querying inbound_plan_ids from the database where last_updated_at >= {last_updated_at}. . .")
@@ -201,7 +206,8 @@ def get_all_inbound_plans_info(marketplaces=['US', 'UK'], inbound_plan_ids='All'
                                                 SELECT inbound_plan_id 
                                                 FROM fulfillment_inbound.inbound_plans
                                                 WHERE marketplace_ids @> ARRAY['{Marketplaces[marketplace].marketplace_id}']
-                                                    AND last_updated_at >= '{last_updated_at}';
+                                                    AND last_updated_at >= '{last_updated_at}'
+                                                    AND tenant_id = {tenants[account]};
                                                 """, 
             )
             inbound_plan_ids = list(query['inbound_plan_id'])
@@ -213,12 +219,13 @@ def get_all_inbound_plans_info(marketplaces=['US', 'UK'], inbound_plan_ids='All'
 
             try:
                 response = FulfillmentInbound(
-                                account=marketplace,
+                                account=f'{account}-{marketplace}',
                                 marketplace=Marketplaces[marketplace],
                                 version='2024-03-20'
                                 ).get_inbound_plan(inboundPlanId=inbound_plan_id)
                 payload = response.payload
                 data = pd.json_normalize(payload)
+                data['tenant_id'] = tenants[account]
                 inbound_data = pd.concat([inbound_data, data], ignore_index=True)
 
             # Doesn't currently support AWD or cross border Amazon Global Logistics
@@ -231,7 +238,7 @@ def get_all_inbound_plans_info(marketplaces=['US', 'UK'], inbound_plan_ids='All'
     return inbound_data
     
 
-def get_all_inbound_plans_shipments(inbound_plan_ids='All', last_updated_at='01-01-1970'):
+def get_all_inbound_plans_shipments(account='Bare Barrel', inbound_plan_ids='All', last_updated_at='01-01-1970'):
     """
     Fetches & combines the full details for a specific shipment within an inbound plan.
     Queries database to retrieve shipment_ids and marketplace. Currently, only available in NA.
@@ -253,6 +260,7 @@ def get_all_inbound_plans_shipments(inbound_plan_ids='All', last_updated_at='01-
                 FROM fulfillment_inbound.inbound_plans_info t1
                 LEFT JOIN amazon_marketplaces t2 ON t1.marketplace_ids[1] = t2.marketplace_id
                 WHERE t1.last_updated_at >= '{last_updated_at}'
+                    AND t1.tenant_id = {tenants[account]}
             """
 
     if inbound_plan_ids != 'All':
@@ -269,18 +277,19 @@ def get_all_inbound_plans_shipments(inbound_plan_ids='All', last_updated_at='01-
         logger.info(f"\tGetting {marketplace} shipments for {inbound_plan_id} - {shipment_id}")
 
         response = FulfillmentInbound(
-                        account=marketplace,
+                        account=f'{account}-{marketplace}',
                         marketplace=Marketplaces[marketplace],
                         version='2024-03-20'
                         ).get_shipment(inbound_plan_id, shipment_id)
         payload = response.payload
         data = pd.json_normalize(payload)
+        data['tenant_id'] = tenants[account]
         inbound_shipments_data = pd.concat([inbound_shipments_data, data], ignore_index=True)
 
     return inbound_shipments_data
 
 
-def update_data(table_name, start_date=None, end_date=None, marketplaces=['US', 'UK']):
+def update_data(table_name, start_date=None, end_date=None, account='Bare Barrel', marketplaces=['US', 'UK']):
     """
     Checks for last X days of updated data.
     Upserts fulfillment inbound daily.
@@ -291,6 +300,7 @@ def update_data(table_name, start_date=None, end_date=None, marketplaces=['US', 
 
     table_names = {
         'fulfillment_inbound.shipments': lambda: get_all_shipments(
+                                            account,
                                             marketplaces,
                                             LastUpdatedAfter=start_date, 
                                             LastUpdatedBefore=end_date,
@@ -298,20 +308,24 @@ def update_data(table_name, start_date=None, end_date=None, marketplaces=['US', 
                                             QueryType='DATE_RANGE'
                                             ),
         'fulfillment_inbound.shipment_items': lambda: get_all_shipment_items(
+                                                account,
                                                 marketplaces,
                                                 LastUpdatedAfter=start_date, 
                                                 LastUpdatedBefore=end_date, 
                                                 QueryType='DATE_RANGE'
                                                 ),
         'fulfillment_inbound.inbound_plans': lambda: get_all_inbound_plans(
+                                                        account,
                                                         marketplaces,
                                                         pageSize=30,
                                                         sortBy='CREATION_TIME'
                                                 ),
         'fulfillment_inbound.inbound_plans_info': lambda: get_all_inbound_plans_info(
+                                                            account,
                                                             last_updated_at=start_date
                                                 ),
         'fulfillment_inbound.inbound_plans_shipments': lambda: get_all_inbound_plans_shipments(
+                                                                account,
                                                                 inbound_plan_ids='All',
                                                                 last_updated_at=start_date
         )
