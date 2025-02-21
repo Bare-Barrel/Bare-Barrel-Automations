f9ade6bfde78a83740b3a21656cf7ca4348a8549
Calvin <calvinmdelrosario@gmail.com>
2025-02-22 02:23:49 +0800

    Added tenant_id in inventory

--- amazon_inventory.py
+++ amazon_inventory.py
@@ -15,26 +15,29 @@ logger_setup.setup_logging(__file__)
 logger = logging.getLogger(__name__)
 
 table_name = 'inventory.fba'
+tenants = postgresql.get_tenants()
+
 
 @throttle_retry()
 @load_all_pages(next_token_param='nextToken')
-def load_all_inventories(marketplace='US', **kwargs):
+def load_all_inventories(account='Bare Barrel', marketplace='US', **kwargs):
     """
     a generator function to return all pages, obtained by NextToken
     """
-    return Inventories(account=marketplace, marketplace=Marketplaces[marketplace]).get_inventory_summary_marketplace(**kwargs)
+    return Inventories(account=f'{account}-{marketplace}', 
+                        marketplace=Marketplaces[marketplace]).get_inventory_summary_marketplace(**kwargs)
 
 
-def get_data(marketplace='US', **kwargs):
+def get_data(account='Bare Barrel', marketplace='US', **kwargs):
     """
     Requests inventory data and returns a pandas dataframe.
     It could could only request a snapshot of current inventory.
     """
-    logger.info(f"Getting inventory for {marketplace} marketplace.")
+    logger.info(f"Getting inventory for {account}-{marketplace}")
 
     combined_data = pd.DataFrame()
 
-    for inventory in load_all_inventories(marketplace, **kwargs):
+    for inventory in load_all_inventories(account, marketplace, **kwargs):
         print(inventory.payload)
         data = pd.json_normalize(inventory.payload['inventorySummaries'], sep='_')
         combined_data = pd.concat([combined_data, data], ignore_index=True)
@@ -45,18 +48,19 @@ def get_data(marketplace='US', **kwargs):
     # manually adding marketplace and date
     combined_data['marketplace'] = marketplace
     combined_data['date'] = dt.datetime.now(pytz.timezone('UTC')).date()
+    combined_data['tenant_id'] = tenants[account]
 
     return combined_data
 
 
-def update_data(marketplaces=['US', 'CA', 'UK']):
+def update_data(account='Bare Barrel', marketplaces=['US', 'CA', 'UK']):
     for marketplace in to_list(marketplaces):
-        data = get_data(marketplace, details=True)
+        data = get_data(account, marketplace, details=True)
         postgresql.upsert_bulk(table_name, data, file_extension='pandas')
 
 
-def create_table(marketplace='US', drop_table_if_exists=False):
-    data = get_data(marketplace, details=True)
+def create_table(account='Bare Barrel', marketplace='US', drop_table_if_exists=False):
+    data = get_data(account, marketplace, details=True)
 
     with postgresql.setup_cursor() as cur:
 
